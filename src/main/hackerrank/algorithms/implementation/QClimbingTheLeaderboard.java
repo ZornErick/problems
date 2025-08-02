2061cc5257e3fb4025be8a7b07b307a8d1905962
erickzorn <erick.nogueira.zorn@gmail.com>
2025-08-02 18:35:39 -0300

    climbing the leaderboard

---
+++ src/main/hackerrank/algorithms/implementation/QClimbingTheLeaderboard.java
@@ -0,0 +1,45 @@
+package main.hackerrank.algorithms.implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Stream;
+
+public class QClimbingTheLeaderboard {
+    public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
+        List<Integer> result = new ArrayList<>();
+
+        List<Integer> rankedCopy = ranked.stream().distinct().toList();
+        for (Integer playerValue : player) {
+            int index = Collections.binarySearch(rankedCopy, playerValue, Comparator.reverseOrder());
+            System.out.println(index);
+            int pos = (index >= 0) ? index + 1 : -index;
+            result.add(pos);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int rankedCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ranked = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .toList();
+
+        int playerCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> player = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .toList();
+
+        List<Integer> result = QClimbingTheLeaderboard.climbingLeaderboard(List.of(100, 90, 90, 80, 75, 60), List.of(50, 65, 77, 90, 102));
+
+        System.out.println(result);
+
+        bufferedReader.close();
+    }
+}
