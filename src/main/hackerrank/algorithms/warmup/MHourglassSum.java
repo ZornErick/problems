4a5cbedfd8760e9e35a219ab50d6afb933816842
ErickZorn <erick.nogueira.zorn@gmail.com>
2025-12-01 15:28:27 -0300

    hourglass sum

---
+++ src/main/hackerrank/algorithms/warmup/MHourglassSum.java
@@ -0,0 +1,53 @@
+package main.hackerrank.algorithms.warmup;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class MHourglassSum {
+    public static int hourglassSum(List<List<Integer>> arr) {
+        int result = Integer.MIN_VALUE;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int sum = arr.get(i).get(j) + arr.get(i).get(j + 1) + arr.get(i).get(j + 2) +
+                        arr.get(i + 1).get(j + 1) +
+                        arr.get(i + 2).get(j) + arr.get(i + 2).get(j + 1) + arr.get(i + 2).get(j + 2);
+
+
+                if (sum > result) {
+                    result = sum;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, 6).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = hourglassSum(arr);
+        System.out.println(result);
+
+        bufferedReader.close();
+    }
+}
