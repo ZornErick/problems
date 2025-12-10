b3bf28fa7c8bfbf1a94597eb0c1966c19b05823f
erickzorn <erick.nogueira.zorn@gmail.com>
2025-12-09 21:59:10 -0300

    circular array rotation

---
+++ src/main/hackerrank/algorithms/implementation/ZCircularArrayRotation.java
@@ -0,0 +1,62 @@
+package main.hackerrank.algorithms.implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class ZCircularArrayRotation {
+    public static List<Integer> circularArrayRotation(List<Integer> a, int k, List<Integer> queries) {
+        List<Integer> result = new ArrayList<>(queries.size());
+
+        k = k % a.size();
+
+        for (int i = 0; i < k; i++) {
+            a.add(0, a.remove(a.size() - 1));
+        }
+
+        for (int query : queries) {
+            result.add(a.get(query));
+        }
+
+
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        int q = Integer.parseInt(firstMultipleInput[2]);
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> queries = IntStream.range(0, q).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine().replaceAll("\\s+$", "");
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = circularArrayRotation(a, k, queries);
+        System.out.println(result);
+
+        bufferedReader.close();
+    }
+}
